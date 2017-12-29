8515074710b681da2d9b427203718f4bb9835c5a
enairkay <34914597+enairkay@users.noreply.github.com>
2017-12-29 12:03:54 +0100

    Create DataTypes.py
    Day 1 of 30 Days of Code in Python 2

---
+++ 30 Days of Code/DataTypes.py
@@ -0,0 +1,16 @@
+#Python 2
+
+# Declare second integer, double, and String variables.
+seci = int(input())
+doubled = float(input())
+sec_c = None
+sec_c = str(raw_input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print int(seci + i)
+# Print the sum of the double variables on a new line.
+print float(doubled + d)
+# Concatenate and print the String variables on a new line
+print s + sec_c
+# The 's' variable above should be printed first.
